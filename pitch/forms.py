102befd9c8d994ecd6d2ac1684bf90f8cd3fc3bd
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-05 16:05:37 +0300

    add project model

---
+++ pitch/forms.py
@@ -0,0 +1,1 @@
+from . models import Profile, Comment, Project
--- pitch/models.py
+++ pitch/models.py
@@ -3,9 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
 class Profile(models.Model):
     profile_photo = models.ImageField('profile/', null=True)
-    bio = models.TextField()
+    jobtitle = models.TextField()
+    email = models.EmailField(unique=True, max_length=254)
+    country = models.TextField(max_length=50, default='Anonymous')
     user = models.ForeignKey(User)
     last_update = models.DateTimeField(auto_now_add=True, null=True)
 
@@ -21,3 +25,52 @@ class Profile(models.Model):
     def delete_profile(self):
         Profile.objects.all().delete()
 
+
+class Project(models.Model):
+    title = models.CharField(max_length=30, null=True)
+    image = models.ImageField(upload_to='image/', null=True)
+    description = models.TextField(max_length=200, null=True)
+    link = models.TextField(max_length=200, null=False)
+    user = models.ForeignKey(User)
+    profile = models.ForeignKey(Profile)
+    design = models.IntegerField(min_value=1, max_value=9, default = 0)
+    Usability = models.IntegerField(min_value=1, max_value=9, default = 0)
+    content = models.IntegerField(min_value=1, max_value=9, default = 0)
+    likes = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-pub_date']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def search_by_user(cls, search_term):
+        images = cls.objects.filter(title__icontains=search_term)
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, project_id):
+        images = cls.objects.get(id=project_id)
+        return images
+    
+class Comment(models.Model):
+    comment = models.CharField(null=True, max_length=5000, verbose_name='name')
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, null=True)
+
+    class Meta:
+        verbose_name = "comments"
+        verbose_name_plural = "comments"
+        ordering = ['-date']
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+        
