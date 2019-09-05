93dce083a7951169b9b73ffdd68d586f87a0aa26
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-05 16:22:38 +0300

    login and logout user fields created in views

--- pitch/views.py
+++ pitch/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Project, Profile, Comments
+
+from django.contrib.auth import authenticate, login, login_required
 
 # Create your views here.
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'home.html', {})
+    else:
+        return redirect('%s?next=%s' % (awwards.settings.LOGIN_URL, request.path))
