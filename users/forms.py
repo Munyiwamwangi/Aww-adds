2ca4f4ee935d9056eb6a2d384dfcb5ff7aac66b2
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-07 20:51:23 +0300

    error rendering in the user registration forms confirmed and perfect, branching to test model posting

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
-class UserRegistrerForm(UserCreationForm):
+class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
 
 	class Meta:
