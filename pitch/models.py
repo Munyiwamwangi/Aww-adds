489ab6c682e8e6026847381194843bfa66a96d75
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-09 15:37:09 +0300

    add a rating and likes model

--- pitch/models.py
+++ pitch/models.py
@@ -33,6 +33,7 @@ class Profile(models.Model):
 class Image(models.Model):
     name = models.CharField(max_length=70)
     profile = models.CharField(max_length=30, blank=True)
+    link = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/', blank=True)
     caption = models.TextField()
 
@@ -47,52 +48,19 @@ class Image(models.Model):
         images = cls.objects.filter(image__name__icontains=search_term)
         return images
 
-from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
-class Comments(models.Model):
-    picture = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comments = models.TextField()
+class Rating(models.Model):
+    design = models.IntegerField(default=0)
+    usability = models.IntegerField(default=0)
+    content = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
 
-    def save_profile(self):
+
+
+    def save_rates(self):
         self.save()
 
-    def delete_profile(self):
+    def delete_rates(self):
         self.delete()
-        
-
-class Profile(models.Model):
-    infor = models.IntegerField(default=0)
-    name=models.CharField( max_length=50, default='Anonym')
-    bio = models.CharField(max_length=80 , blank=True)
-    profile_picture = models.ImageField(upload_to='images/', blank=True)
-
-    class Meta:
-        ordering = ['profile_picture']
-
-    def save_profile(self):
-        self.save()
-
-    def delete_profile(self):
-        self.delete()
-
-
-class Image(models.Model):
-    name = models.CharField(max_length=70)
-    profile = models.CharField(max_length=30, blank=True)
-    image = models.ImageField(upload_to='images/', blank=True)
-    caption = models.TextField()
-
-    def save_image(self):
-        self.save()
-
-    def delete_image(self):
-        self.delete()
-
-    @classmethod
-    def search_by_name(cls, search_term):
-        images = cls.objects.filter(image__name__icontains=search_term)
-        return images
-
