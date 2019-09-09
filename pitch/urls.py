77c204929105cbb3918b2013cb76458e5e70bfe6
Munyiwamwangi <josseymunyi@gmail.com>
2019-09-09 09:25:09 +0300

    adding routing to redirect to image

--- pitch/urls.py
+++ pitch/urls.py
@@ -1,20 +1,26 @@
-from django.urls import path
-from .import views
-from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-    UserPostListView
-)
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('',views.home, name='pitches'),
-    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('about/', views.about, name='about'),
+    url('^$', views.index, name='index'),
+    url(r'posts/$', views.welcome, name='welcome'),
+    url(r'^search/', views.search_image, name='search_image'),
+    url(r'^accunts/profile/$', views.edit_profile_info, name='edit_profile_info'),
+    url(r'^upload/$', views.Photo, name='uploadPhoto'),
+    url(r'comment/(\d+)', views.comment, name='comment'),
+    url(r'^profile_info/$', views.profile, name='profile_info'),
+
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',
+    #     views.past_days_images, name='pastImages'),
+    # url(r'^search/', views.search_results, name='search_results'),
 ]
+
+#To serve uploaded images on the development server we need to configure our urls.py to register the MEDIA_ROOT route.
+# add to the urlpatterns images static route that references the location to the uploaded files.
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
